70d16a811c61003fb983dc229d64d93d889fa6c4
yaysunshine <merida1592@gmail.com>
2015-03-11 15:22:48 -0700

    banging my head and trying so much code

--- app/helpers/oauth.rb
+++ app/helpers/oauth.rb
@@ -9,12 +9,17 @@ end
 
 def request_token
   if not session[:request_token]
+    p session[:request_token]
     host_and_port = request.host
+    p host_and_port
     host_and_port << ":9393" if request.host == "localhost"
+    p host_and_port
 
     session[:request_token] = oauth_consumer.get_request_token(
       :oauth_callback => "http://#{host_and_port}/auth"
     )
+    p session[:request_token]
   end
   session[:request_token]
+  p session[:request_token]
 end
