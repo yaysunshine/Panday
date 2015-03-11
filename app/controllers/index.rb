b723ccbc18e8beb1410cde9ce0d64173dd88bafb
yaysunshine <merida1592@gmail.com>
2015-03-11 01:34:37 -0700

    working off of found code

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -24,16 +24,59 @@ get '/:user_id/profile' do
 end
 
 get '/auth' do
-  @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
-  session.delete(:request_token)
-  erb :profile
+  client = TwitterOAuth::Client.new(
+    :consumer_key => ENV['CONSUMER_KEY'],
+    :consumer_secret => ENV['CONSUMER_SECRET'],
+    )
+
+  request_token = client.request_token(
+    :oauth_callback => base_path + '/sign_in',
+    )
+  session[:request_token] = request_token
+
+  redirect (request_token.authorize_url)
 end
 
+#   @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
+#   session.delete(:request_token)
+#   erb :profile
+# end
+
 get '/sign_in' do
-  redirect request_token.authorize_url
+  client = TwitterOAuth::Client.new(
+    :consumer_key => ENV['CONSUMER_KEY'],
+    :consumer_secret => ENV['CONSUMER_SECRET'],
+    )
+  access_token = client.authorize(
+    session[:request_token].token,
+    session[:request_token].secret,
+    :oauth_verifier => params[:oauth_verifier]
+    )
+  session[:oauth_verifier] = params[:oauth_verifier]
+  redirect '/'
+end
+#   redirect request_token.authorize_url
+# end
+
+get '/tweet' do
+  client = TwitterOAuth::Client.new(
+    :consumer_key => ENV['CONSUMER_KEY'],
+    :consumer_secret => ENV['CONSUMER_SECRET'],
+    )
+  access_token = client.authorize(
+    session[:request_token].token,
+    session[:request_token].secret,
+    :oauth_verifier => params[:oauth_verifier]
+    )
+  tweet = "#{params['panda']}"
+  client.update(tweet)
 end
 
 get '/sign_out' do
-  session.clear
+  session[:request_token] = nil
+  session[:oauth_verifier] = nil
   redirect '/'
 end
+#   session.clear
+#   redirect '/'
+# end
--- app/helpers/oauth.rb
+++ app/helpers/oauth.rb
@@ -1,20 +1,20 @@
-def oauth_consumer
-  raise RuntimeError, "You must set CONSUMER_KEY and CONSUMER_SECRET in your server environment." unless ENV['CONSUMER_KEY'] && ENV['CONSUMER_SECRET']
-  @consumer ||= OAuth::Consumer.new(
-    ENV['CONSUMER_KEY'],
-    ENV['CONSUMER_SECRET'],
-    :site => "https://api.twitter.com"
-    )
-end
+# def oauth_consumer
+#   raise RuntimeError, "You must set CONSUMER_KEY and CONSUMER_SECRET in your server environment." unless ENV['CONSUMER_KEY'] && ENV['CONSUMER_SECRET']
+#   @consumer ||= OAuth::Consumer.new(
+#     ENV['CONSUMER_KEY'],
+#     ENV['CONSUMER_SECRET'],
+#     :site => "https://api.twitter.com"
+#     )
+# end
 
-def request_token
-  if not session[:request_token]
-    host_and_port = request.host
-    host_and_port << ":9393" if request.host == "localhost"
+# def request_token
+#   if not session[:request_token]
+#     host_and_port = request.host
+#     host_and_port << ":9393" if request.host == "localhost"
 
-    session[:request_token] = oauth_consumer.get_request_token(
-      :oauth_callback => "http://#{host_and_port}/auth"
-    )
-  end
-  session[:request_token]
-end
+#     session[:request_token] = oauth_consumer.get_request_token(
+#       :oauth_callback => "http://#{host_and_port}/auth"
+#     )
+#   end
+#   session[:request_token]
+# end
