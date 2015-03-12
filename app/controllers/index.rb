e1eded3992b150cd0af5dfd5a453d1ed2f11cec0
yaysunshine <merida1592@gmail.com>
2015-03-11 17:04:16 -0700

    starting oauth over

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,22 +1,17 @@
+require 'sinatra'
+require 'omniauth-twitter'
+
+
+
 get '/' do
   erb :sign_in
 end
 
 get '/sign_in' do
-  redirect request_token.authorize_url
 end
 
 get '/sign_out' do
-  session.clear
-  redirect '/'
 end
 
 get '/auth' do
-  @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
-  session.delete(:request_token)
-   user = User.where(oauth_token: params[:oauth_token], oauth_secret: params[:oauth_secret])
-   session[:user_id] =user.id
-
-  erb :profile
-
 end
