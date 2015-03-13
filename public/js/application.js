6543ed1e5d89625d27bad90e88f57072d017c574
yaysunshine <merida1592@gmail.com>
2015-03-12 17:03:54 -0700

    add date and colors

--- public/js/application.js
+++ public/js/application.js
@@ -1,25 +1,5 @@
 $(document).ready(function(){
 
-  $('#signin').on("click", function(event) {
-    event.preventDefault();
-
-    var request = $.ajax({
-      url: "/color",
-      type: "POST",
-      dataType: "json"
-    });
-
-    request.done(function(cellData){
-      updateCell(cellData);
-    });
-
-  });
-
-  var updateCell = function(options) {
-    var cell = $("li:nth-child(" + options.cell + ")");
-    cell.css("background", options.color);
-  };
-
 
 
 
